b48bc8a54ddb1567b2c545a0eb69897e48e6edeb
mspake <morgana.spake@gmail.com>
2019-04-16 12:18:30 -0700

    weather added

--- server.js
+++ server.js
@@ -9,29 +9,48 @@ const app = express();
 app.use(cors());
 
 function Geo_data_object(search_query, formatted_query, latitude, longitude) {
-    this.search_query = search_query;
-    this.formatted_query = formatted_query;
-    this.latitude = latitude;
-    this.longitude = longitude;
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.latitude = latitude;
+  this.longitude = longitude;
+}
+
+function Weather_data_object(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
 }
 
 app.get('/location', (request, response) => {
-    response.send(search_location(request.query.data));
+  response.send(search_location(request.query.data));
+})
+
+app.get('/weather', (request, response) => {
+  response.send(search_weather());
 })
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
 function search_location(front_end_query) {
-    const search_query = front_end_query;
+  const search_query = front_end_query;
 
-    const geo_data = require('./data/geo.json');
-    const formatted_query = geo_data.results[0].formatted_address;
-    const latitude = geo_data.results[0].geometry.location.lat;
-    const longitude = geo_data.results[0].geometry.location.lng;
+  const geo_data = require('./data/geo.json');
+  const formatted_query = geo_data.results[0].formatted_address;
+  const latitude = geo_data.results[0].geometry.location.lat;
+  const longitude = geo_data.results[0].geometry.location.lng;
 
-    const location_object = new Geo_data_object(search_query, formatted_query, latitude, longitude);
+  const location_object = new Geo_data_object(search_query, formatted_query, latitude, longitude);
 
-    return location_object;
+  return location_object;
 }
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+function search_weather() {
+  const weather_data = require('./data/darksky.json');
+  const forecast = weather_data.daily.data[0].summary;
+  const time = weather_data.daily.data[0].time;
+
+  const weather_object = new Weather_data_object(forecast, time);
+
+  return weather_object;
+}
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
